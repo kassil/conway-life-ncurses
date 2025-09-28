6e10e200980f8e8dbd2372511d4b6deceaa984f1
Kevin Kassil <k.kassil@gmail.com>
2025-09-27 21:46:30 -0400

    Random initial state
    Use an off-screen window to perform each update.

--- main.rs
+++ main.rs
@@ -4,10 +4,9 @@
 extern crate ncurses;
 
 use ncurses::*;
+use rand::random;
 use std::cmp::{max, min};
-//use std::fs;
-//use std::io;
-use std::{thread, time};
+use std::time;
 
 fn count_live_neighbours(win: WINDOW, y_ctr: i32, x_ctr: i32) -> i32 {
     let mut n_live: i32 = 0;
@@ -27,7 +26,7 @@ fn count_live_neighbours(win: WINDOW, y_ctr: i32, x_ctr: i32) -> i32 {
 }
 
 fn canvas_get_cell(win: WINDOW, y: i32, x: i32) -> char {
-    char::from_u32(mvwinch(win, y, x) & 0xFF).unwrap()
+    char::from_u32(mvwinch(win, y, x) & 0xFF).unwrap_or(' ')
 }
 
 fn canvas_put_cell(win: WINDOW, y: i32, x: i32, cell_state: char) {
@@ -41,31 +40,48 @@ fn main() {
     let win = stdscr();
     keypad(win, true);
     scrollok(win, true);
+    nodelay(win, true);
+    let win2 = newwin(getmaxy(win), getmaxx(win), 0, 0);
+    keypad(win2, true);
+    scrollok(win2, true);
+    nodelay(win2, true);
     start_color();
     init_pair(1, COLOR_WHITE, COLOR_BLUE);      // Regular files
     init_pair(2, COLOR_YELLOW, COLOR_BLUE);     // Directories
 
-    // let mut all_done: bool = false;
+    let mut delay_ms = 100;
 
-    struct UpdateCell {
-        x: i32,
-        y: i32,
-        state: char,
-    }
-    let mut cell_updates = Vec::new(); ////Vec<UpdateCell>;
-
-    for j in 0..3 {
-        for i in 0..3 {
-            let y = getmaxy(win)/2 + j;
-            let x = getmaxx(win)/2 + i;
-            canvas_put_cell(win, y, x, 'O');
+    let initial_mode = 1;
+    match initial_mode {
+        0 => {
+            for j in -3..4 {
+                for i in -3..4 {
+                    if i!=0 && j!=0 {
+                        let y = getmaxy(win)/2 + j;
+                        let x = getmaxx(win)/2 + i;
+                        canvas_put_cell(win, y, x, 'O');
+                        canvas_put_cell(win2, y, x, 'O');
+                    }
+                }
+            }
+        }
+        1 => {
+            for y in 0..getmaxy(win) {
+                for x in 0..getmaxx(win) {
+                    // Roll a dice with a 50% chance using a random boolean
+                    if random() {
+                        mvwaddch(win, y, x, 'O' as u32 );
+                    }
+                }
+            }
+        }
+        _ => {
         }
     }
-    wrefresh(win);
 
     loop {
-
-        std::thread::sleep(time::Duration::from_millis(200));
+        wrefresh(win);
+        std::thread::sleep(time::Duration::from_millis(delay_ms));
 
         for y in 0..getmaxy(win) {
             for x in 0..getmaxx(win) {
@@ -76,39 +92,45 @@ fn main() {
                     // Live
                     if n_live < 2 {
                         // Underpopulation
-                        cell_updates.push(UpdateCell{x: x, y: y, state: ' '});
+                        canvas_put_cell(win2, y, x, ' ');
                     }
                     else if n_live > 3 {
                         // Overpopulation
-                        cell_updates.push(UpdateCell{x: x, y: y, state: ' '});
+                        canvas_put_cell(win2, y, x, ' ');
                     }
                 }
                 else if cell == ' ' && n_live == 3 {
                     // Reproduction
-                    cell_updates.push(UpdateCell{x: x, y: y, state: 'O'});
+                    canvas_put_cell(win2, y, x, 'O');
                 }
-
             }
         }
         // Draw changes after considering all cells
-        for cell in &cell_updates {
-            canvas_put_cell(win, cell.y, cell.x, cell.state);
-        }
-        cell_updates.clear();
-
-        wrefresh(win);
+        overwrite(win2, win);
 
         // Handle input
-        //let ch = wgetch(win);
-        // match ch {
-        //     KEY_ESC => {
-        //         break;
-        //     }
-        //     _ => {
-        //     }
-        // }
+        const KEY_Q: i32 = 'q' as i32;
+        const KEY_ESC: i32 = 27;
+        match wgetch(win) {
+            KEY_RESIZE => {
+                let new_rows = LINES();
+                let new_cols = COLS();
+                wresize(win, new_rows, new_cols);
+                wresize(win2, new_rows, new_cols);
+            }
+            // Escape or 'q' to quit
+            KEY_Q | KEY_ESC => {
+                break;
+            }
+            val if val == '+' as i32 => {
+                delay_ms = max(delay_ms * 4 / 5, 10);
+            }
+            val if val == '-' as i32 => {
+                delay_ms = delay_ms * 5 / 4;
+            }
+            _ => {}
+        }
     }
 
     endwin();
-    return;
 }
